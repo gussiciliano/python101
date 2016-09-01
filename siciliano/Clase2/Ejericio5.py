f9a8d3a13af6f02af5d3be7d4a9a3f194086540b
Gustavo <gussiciliano@gmail.com>
2016-09-01 00:38:27 -0300

    Falta el último. GHS

---
+++ siciliano/Clase2/Ejericio5.py
@@ -0,0 +1,5 @@
+#5- Se debera generar un sistema que mantenga en memoria datos de una agenda.
+#    - El programa mostrara las opciones> agregar, editar, borrar, mostrar y salir
+#    agregar, agenda un contacto (email, telefono, nombre, domicilio, edad y dni)
+#    editar, permite modificar cualquiera de los contactos seleccionando su email.
+#    borrar, elimina un contacto.
